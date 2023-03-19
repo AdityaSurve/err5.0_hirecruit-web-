0b11cfe37a397e01e3181a4dca5b0b8661b9aaca
ShehalShah <102235406+ShehalShah@users.noreply.github.com>
2023-03-19 12:01:01 +0530

    shehal commit 14

--- src/pages/JobDesc.jsx
+++ src/pages/JobDesc.jsx
@@ -66,14 +66,20 @@ const JobDesc = () => {
                 </div>
                 <div className='flex justify-between'>
                     <div class="px-6 pt-4 pb-2">
-                        <span class="inline-block text-gray-200 rounded-xl px-3 py-1 text-sm font-semibold bg-blue-500 mr-2 mb-2">
+                        <span class="inline-block text-gray-200 rounded-xl px-3 py-1 text-sm font-semibold bg-green-500 mr-2 mb-2">
                             {job.type}
                         </span>
-                        <span class="inline-block text-gray-200 rounded-xl px-3 py-1 text-sm font-semibold bg-blue-500 mr-2 mb-2">
+                        <span class="inline-block text-gray-200 rounded-xl px-3 py-1 text-sm font-semibold bg-green-500 mr-2 mb-2">
                             {job.location}
                         </span>
+                        {fireuser?.skills?.map((skill) => {
+                            return (<span className="inline-block text-gray-200 rounded-xl px-3 py-1 text-sm font-semibold bg-blue-500 mr-2 mb-2">
+                                {skill}
+                            </span>)
+                        })}
                     </div>
                     <div>
+                        
                         {applied||fireuser?.appliedfor?.includes(jobId)?<button
                             // onClick={() => navigate(`/job/${job.job_id}`)}
                             className='bg-green-500 text-white font-semibold rounded-xl p-1 border-2 border-green-600 hover:bg-green-700 mr-10 flex items-center gap-2' >Applied ! <AiOutlineCheckCircle />
@@ -82,6 +88,7 @@ const JobDesc = () => {
                             className='bg-blue-500 text-white font-semibold rounded-xl p-1 border-2 border-blue-600 hover:bg-blue-700 mr-10' onClick={handleApply}>Apply Now
                         </button>}
                     </div>
+                    
                 </div>
             </div>
             <hr></hr><hr></hr>
