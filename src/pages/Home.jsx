7ffd0178ce05bec97aadd857d141d175541252ed
AdityaSurve <surveaditya521@gmail.com>
2023-06-15 05:11:29 +0530

    minor2

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,115 +1,145 @@
-import React,{useEffect,useState} from 'react'
-import { AiOutlineSearch } from 'react-icons/ai'
-import TrendingCard from '../components/TrendingCard'
-import './Home.css'
-import axios from 'axios'
-import { app, database, storage } from '../firebase-config'
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, GoogleAuthProvider, signInWithPopup, onAuthStateChanged, signOut, updateProfile } from "firebase/auth";
-import { collection, addDoc, getDocs, doc, updateDoc, deleteDoc,setDoc, onSnapshot, query, where } from "firebase/firestore";
+import React, { useEffect, useState } from "react";
+import { AiOutlineSearch } from "react-icons/ai";
+import TrendingCard from "../components/TrendingCard";
+import "./Home.css";
+import axios from "axios";
+import { app, database, storage } from "../firebase-config";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  GoogleAuthProvider,
+  signInWithPopup,
+  onAuthStateChanged,
+  signOut,
+  updateProfile,
+} from "firebase/auth";
+import {
+  collection,
+  addDoc,
+  getDocs,
+  doc,
+  updateDoc,
+  deleteDoc,
+  setDoc,
+  onSnapshot,
+  query,
+  where,
+} from "firebase/firestore";
 import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
-import { async } from '@firebase/util'
-import { useNavigate } from 'react-router-dom'
+import { async } from "@firebase/util";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
-  const [search,  setSearch] = useState('')
-  const navigate = useNavigate()
+  const [search, setSearch] = useState("");
+  const navigate = useNavigate();
 
-  const [jobs, setjobs] = useState([])
-  const collectionRef = collection(database, 'job');
+  const [jobs, setjobs] = useState([]);
+  const collectionRef = collection(database, "job");
   useEffect(() => {
     getJobs();
-  }, [])
+  }, []);
 
-  const getJobs=async()=>{
+  const getJobs = async () => {
     const collectionRef = collection(database, "job");
     onSnapshot(collectionRef, (hacklist) => {
       setjobs(hacklist.docs);
-    })
-  }
-  
-
-  const handleAxios=()=>{
-    console.log("hello");
-  const options = {
-    method: 'GET',
-    url: 'https://jsearch.p.rapidapi.com/search',
-    params: { query: 'developer', page: '1', num_pages: '1' },
-    headers: {
-      'X-RapidAPI-Key': '48334e4faemsh146b66580f9c961p13d654jsnfb3484c3b23a',
-      'X-RapidAPI-Host': 'jsearch.p.rapidapi.com'
-    }
+    });
   };
 
-  axios.request(options).then(function (response) {
-    console.log(response.data.data);
-    const arr=response.data.data;
-    for (let i = 7; i < arr.length; i++) {
-      const element = arr[i];
-      setDoc(doc(database, "job", element.job_id), {
-        jobid:element.job_id,
-        title:element?.job_title?element.job_title:element.job_job_title,
-        logo:element?.employer_logo,
-        ename:element?.employer_name,
-        desc:element?.job_description,
-        type:element?.job_employment_type,
-        skills:element?.job_required_skills,
-        location:element?.job_city+" "+element?.job_state+" "+element?.job_country,
-        resp:element.job_highlights?.Responsibilities,
-        qual:element.job_highlights?.Qualifications
-      })
-      console.log("done");
-    }
-  }).catch(function (error) {
-    console.error(error);
-  });
-  }
+  const handleAxios = () => {
+    console.log("hello");
+    const options = {
+      method: "GET",
+      url: "https://jsearch.p.rapidapi.com/search",
+      params: { query: "developer", page: "1", num_pages: "1" },
+      headers: {
+        "X-RapidAPI-Key": "48334e4faemsh146b66580f9c961p13d654jsnfb3484c3b23a",
+        "X-RapidAPI-Host": "jsearch.p.rapidapi.com",
+      },
+    };
 
+    axios
+      .request(options)
+      .then(function (response) {
+        console.log(response.data.data);
+        const arr = response.data.data;
+        for (let i = 7; i < arr.length; i++) {
+          const element = arr[i];
+          setDoc(doc(database, "job", element.job_id), {
+            jobid: element.job_id,
+            title: element?.job_title
+              ? element.job_title
+              : element.job_job_title,
+            logo: element?.employer_logo,
+            ename: element?.employer_name,
+            desc: element?.job_description,
+            type: element?.job_employment_type,
+            skills: element?.job_required_skills,
+            location:
+              element?.job_city +
+              " " +
+              element?.job_state +
+              " " +
+              element?.job_country,
+            resp: element.job_highlights?.Responsibilities,
+            qual: element.job_highlights?.Qualifications,
+          });
+          console.log("done");
+        }
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  };
 
   return (
     <div>
-      
-      <div className='sm:grid grid-cols-2'>
-        <div className='flex flex-col text-3xl font-bold  justify-center items-center '>
-          <div className='text-left'>
-            <p className=''>Find Your Dream Job Faster</p>
+      <div className="sm:grid grid-cols-2">
+        <div className="flex flex-col text-3xl font-bold  justify-center items-center ">
+          <div className="text-left">
+            <p className="">Find Your Dream Job Faster</p>
           </div>
-          <div className=''>
-
-            <div className='flex justify-center items-center border-2 rounded-full my-3 p-1'>
-
-              <input placeholder='Search...' className='outline-none pl-2' type='text' 
-              onChange={(e) => setSearch(e.target.value)}/>
-              <div className='bg-gradient-to-r from-cyan-500 to-blue-500 text-white p-3 rounded-full cursor-pointer '
-              onClick={() => navigate(`/search/${search}`)}>
+          <div className="">
+            <div className="flex justify-center items-center border-2 rounded-full my-3 p-1">
+              <input
+                placeholder="Search..."
+                className="outline-none pl-2"
+                type="text"
+                onChange={(e) => setSearch(e.target.value)}
+              />
+              <div
+                className="bg-gradient-to-r from-cyan-500 to-blue-500 text-white p-3 rounded-full cursor-pointer "
+                onClick={() => navigate(`/search/${search}`)}
+              >
                 <AiOutlineSearch />
               </div>
             </div>
           </div>
         </div>
         <div>
-          <img src='http://get2.imglarger.com:8889/results/m0ICyKe6_4x.jpg' alt='/' />
+          <img
+            src="http://get2.imglarger.com:8889/results/m0ICyKe6_4x.jpg"
+            alt="/image"
+          />
         </div>
       </div>
 
-
-      <div className='flex justify-center items-center flex-col '>
-        <div className='border-2 m-3 p-6 rounded-xl'>
-
-        <div className='text-xl font-semibold my-2 text-center'>Recommended Jobs</div>
-        <hr></hr>
-        <div className='flex flex-col overflow-y-scroll max-h-[500px]'>
-          
-          {jobs.map((item) => {
-            
-          return (
-            <TrendingCard job={item.data()}/>
-          );
-        }, [])}
+      <div className="flex justify-center items-center flex-col ">
+        <div className="border-2 m-3 p-6 rounded-xl">
+          <div className="text-xl font-semibold my-2 text-center">
+            Recommended Jobs
+          </div>
+          <hr></hr>
+          <div className="flex flex-col overflow-y-scroll max-h-[500px]">
+            {jobs.map((item) => {
+              return <TrendingCard job={item.data()} />;
+            }, [])}
+          </div>
         </div>
       </div>
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
--- src/pages/JobDesc.jsx
+++ src/pages/JobDesc.jsx
@@ -1,100 +1,161 @@
-import React, { useState, useEffect } from 'react'
-import { useParams } from 'react-router-dom'
-import { app, database, storage } from '../firebase-config'
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, GoogleAuthProvider, signInWithPopup, onAuthStateChanged, signOut, updateProfile } from "firebase/auth";
-import { collection, addDoc, getDoc, getDocs, doc, updateDoc, deleteDoc, setDoc, onSnapshot, query, where, arrayUnion } from "firebase/firestore";
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { app, database, storage } from "../firebase-config";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  GoogleAuthProvider,
+  signInWithPopup,
+  onAuthStateChanged,
+  signOut,
+  updateProfile,
+} from "firebase/auth";
+import {
+  collection,
+  addDoc,
+  getDoc,
+  getDocs,
+  doc,
+  updateDoc,
+  deleteDoc,
+  setDoc,
+  onSnapshot,
+  query,
+  where,
+  arrayUnion,
+} from "firebase/firestore";
 import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
-import { UserAuth } from '../contexts/AuthContext'
-import { AiOutlineCheck, AiOutlineCheckCircle } from 'react-icons/ai';
+import { UserAuth } from "../contexts/AuthContext";
+import { AiOutlineCheck, AiOutlineCheckCircle } from "react-icons/ai";
 
 const JobDesc = () => {
-    const [applied, setapplied] = useState(false)
-    const { jobId } = useParams()
-    const [job, setjob] = useState({})
-    const {user,logOut} = UserAuth()
-    const [fireuser, setfireuser] = useState({})
-    useEffect(() => {
-        getDeets();
-        getfireuser();
-    }, [])
+  const [applied, setapplied] = useState(false);
+  const { jobId } = useParams();
+  const [job, setjob] = useState({});
+  const { user, logOut } = UserAuth();
+  const [fireuser, setfireuser] = useState({});
+  const [JFS, setJFS] = useState("");
+  useEffect(() => {
+    getDeets();
+    getfireuser();
+  }, []);
 
-    const getfireuser = async () => {
-        const docRef = doc(database, "users", user.uid);
-        const docSnap = await getDoc(docRef);
-        setfireuser(docSnap.data())
-    }
+  const getfireuser = async () => {
+    const docRef = doc(database, "users", user.uid);
+    const docSnap = await getDoc(docRef);
+    setfireuser(docSnap.data());
+  };
 
-    const getDeets = async () => {
-        const docRef = doc(database, "job", jobId);
-        const docSnap = await getDoc(docRef);
-        setjob(docSnap.data())
-    }
+  const getDeets = async () => {
+    const docRef = doc(database, "job", jobId);
+    const docSnap = await getDoc(docRef);
+    setjob(docSnap.data());
+  };
 
-    let longString = job.desc
-    let newString = longString?.split('•').join('.<br>');
+  let longString = job.desc;
+  let newString = longString?.split("•").join(".<br>");
 
-    const handleApply=()=>{
-        const jobtoupdate = doc(database, 'job', jobId)
-        updateDoc(jobtoupdate, {
-          applicants:arrayUnion(user.uid)
-        })
-        const usertoupdate = doc(database, 'users', user.uid)
-        updateDoc(usertoupdate, {
-          appliedfor:arrayUnion(jobId)
-        })
-        setapplied(true)
-        
-    }
+  const joblist = job.skills;
+  const customerList = fireuser.skills;
+  const handleApply = () => {
+    const jobtoupdate = doc(database, "job", jobId);
+    updateDoc(jobtoupdate, {
+      applicants: arrayUnion(user.uid),
+    });
+    const usertoupdate = doc(database, "users", user.uid);
+    updateDoc(usertoupdate, {
+      appliedfor: arrayUnion(jobId),
+    });
+    setapplied(true);
+    sendListsToServer(joblist, customerList);
+  };
 
-    return (
-        <div className='min-h-[630px] mx-[50px]'>
-            <div class=" rounded-xl overflow-hidden bg-white   justify-between items-center my-1 mx-1 p-3">
-                <div class="px-6 py-4 flex items-center">
-                    <img
-                        className='w-[150px]  object-cover'
-                        src={job.logo ? job.logo : "https://img-prod-cms-rt-microsoft-com.akamaized.net/cms/api/am/imageFileData/RE4wB6L?ver=72af"}
-                        alt="Sunset in the mountains"
-                    />
-                    <div className='flex flex-col ml-5'>
-                        <div class="font-bold text-3xl mb-3">{job.title}</div>
-                        <div className='flex font-semibold text-green-600 h-min  mb-2 text-sm bg-green-200 border-2 border-green-500  rounded-xl px-2'>{job.ename}</div>
-                    </div>
-                </div>
-                <div>
+  const sendListsToServer = async (joblist, customerList) => {
+    fetch("/compare", {
+      method: "POST",
+      body: JSON.stringify({ list1: joblist, list2: customerList }),
+      headers: { "Content-Type": "application/json" },
+    })
+      .then((response) => response.json())
+      .then((JFS) => {
+        // setJFS(JFS);
+        // const usertoupdate = doc(database, "jfs", user.uid + jobId);
+        setDoc(doc(database, "jfs", user.uid + jobId), {
+          jfs: JFS.JFS,
+        });
+        console.log(JFS);
+      })
+      .catch((error) => {
+        console.error("Error sending lists to server:", error);
+      });
+  };
 
-
-                </div>
-                <div className='flex justify-between'>
-                    <div class="px-6 pt-4 pb-2">
-                        <span class="inline-block text-gray-200 rounded-xl px-3 py-1 text-sm font-semibold bg-green-500 mr-2 mb-2">
-                            {job.type}
-                        </span>
-                        <span class="inline-block text-gray-200 rounded-xl px-3 py-1 text-sm font-semibold bg-green-500 mr-2 mb-2">
-                            {job.location}
-                        </span>
-                        {fireuser?.skills?.map((skill) => {
-                            return (<span className="inline-block text-gray-200 rounded-xl px-3 py-1 text-sm font-semibold bg-blue-500 mr-2 mb-2">
-                                {skill}
-                            </span>)
-                        })}
-                    </div>
-                    <div>
-                        
-                        {applied||fireuser?.appliedfor?.includes(jobId)?<button
-                            // onClick={() => navigate(`/job/${job.job_id}`)}
-                            className='bg-green-500 text-white font-semibold rounded-xl p-1 border-2 border-green-600 hover:bg-green-700 mr-10 flex items-center gap-2' >Applied ! <AiOutlineCheckCircle />
-                        </button>:<button
-                            // onClick={() => navigate(`/job/${job.job_id}`)}
-                            className='bg-blue-500 text-white font-semibold rounded-xl p-1 border-2 border-blue-600 hover:bg-blue-700 mr-10' onClick={handleApply}>Apply Now
-                        </button>}
-                    </div>
-                    
-                </div>
+  return (
+    <div className="min-h-[630px] mx-[50px]">
+      <div class=" rounded-xl overflow-hidden bg-white   justify-between items-center my-1 mx-1 p-3">
+        <div class="px-6 py-4 flex items-center">
+          <img
+            className="w-[150px]  object-cover"
+            src={
+              job.logo
+                ? job.logo
+                : "https://img-prod-cms-rt-microsoft-com.akamaized.net/cms/api/am/imageFileData/RE4wB6L?ver=72af"
+            }
+            alt="Sunset in the mountains"
+          />
+          <div className="flex flex-col ml-5">
+            <div class="font-bold text-3xl mb-3">{job.title}</div>
+            <div className="flex font-semibold text-green-600 h-min  mb-2 text-sm bg-green-200 border-2 border-green-500  rounded-xl px-2">
+              {job.ename}
             </div>
-            <hr></hr><hr></hr>
-            <div dangerouslySetInnerHTML={{__html: newString}} className='mt-6'></div>
+          </div>
         </div>
-    )
-}
+        <div></div>
+        <div className="flex justify-between">
+          <div class="px-6 pt-4 pb-2">
+            <span class="inline-block text-gray-200 rounded-xl px-3 py-1 text-sm font-semibold bg-green-500 mr-2 mb-2">
+              {job.type}
+            </span>
+            <span class="inline-block text-gray-200 rounded-xl px-3 py-1 text-sm font-semibold bg-green-500 mr-2 mb-2">
+              {job.location}
+            </span>
+            {fireuser?.skills?.map((skill) => {
+              return (
+                <span className="inline-block text-gray-200 rounded-xl px-3 py-1 text-sm font-semibold bg-blue-500 mr-2 mb-2">
+                  {skill}
+                </span>
+              );
+            })}
+          </div>
+          <div>
+            {applied || fireuser?.appliedfor?.includes(jobId) ? (
+              <button
+                // onClick={() => navigate(`/job/${job.job_id}`)}
+                className="bg-green-500 text-white font-semibold rounded-xl p-1 border-2 border-green-600 hover:bg-green-700 mr-10 flex items-center gap-2"
+              >
+                Applied ! <AiOutlineCheckCircle />
+              </button>
+            ) : (
+              <button
+                // onClick={() => navigate(`/job/${job.job_id}`)}
+                className="bg-blue-500 text-white font-semibold rounded-xl p-1 border-2 border-blue-600 hover:bg-blue-700 mr-10"
+                onClick={handleApply}
+              >
+                Apply Now
+              </button>
+            )}
+          </div>
+        </div>
+      </div>
+      <hr></hr>
+      <hr></hr>
+      <div
+        dangerouslySetInnerHTML={{ __html: newString }}
+        className="mt-6"
+      ></div>
+    </div>
+  );
+};
 
-export default JobDesc
+export default JobDesc;
